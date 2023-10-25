e5a882c2059da95f608efde7f8910b888b3b20ff
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-10-25 15:24:37 -0500

    456456

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Button, Alert, Breadcrumb, Card } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card className='mb-3'>
+          <Card.Img src="https://picsum.photos/200/100"/>
+          <Card.Body>
+            <Card.Title>Card Example</Card.Title>
+            <Card.Text>This an example of react boostrap cards</Card.Text>
+            <Button>This is button</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <Breadcrumb.Item>Test1</Breadcrumb.Item>
+          <Breadcrumb.Item>Test2</Breadcrumb.Item>
+          <Breadcrumb.Item active style={{color:"white"}}>Test3</Breadcrumb.Item>
+        </Breadcrumb>
+        <Alert variant='primary'>This is a button</Alert>
+        <Button>This is button</Button>
       </header>
     </div>
   );
